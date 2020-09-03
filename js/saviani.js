42cbbba2089c4ab9940d4fb59567b5e9a6942ccf
josepaulo95 <fernandes.josepaulo95@gmail.com>
2020-09-03 00:33:29 -0300

    feat: model aluno com campos certos e fazendo busca por nomes

--- js/saviani.js
+++ js/saviani.js
@@ -33,6 +33,7 @@ var App = new Vue({
               numeric: true
             },
             {
+              searchable: true,
               field: 'nome',
               label: 'Nome',
             },
@@ -51,35 +52,27 @@ var App = new Vue({
         "alunos": {
           label: "Alunos",
           dados: [
-            { 'id': 1, 'first_name': 'Jesse', 'last_name': 'Simmons', 'date': '2016-10-15 13:43:27', 'gender': 'Male' },
-            { 'id': 2, 'first_name': 'John', 'last_name': 'Jacobs', 'date': '2016-12-15 06:00:53', 'gender': 'Male' },
-            { 'id': 3, 'first_name': 'Tina', 'last_name': 'Gilbert', 'date': '2016-04-26 06:26:28', 'gender': 'Female' },
-            { 'id': 4, 'first_name': 'Clarence', 'last_name': 'Flores', 'date': '2016-04-10 10:28:46', 'gender': 'Male' },
-            { 'id': 5, 'first_name': 'Anne', 'last_name': 'Lee', 'date': '2016-12-06 14:38:38', 'gender': 'Female' }
+            { 'id': 1, 'nome': 'Jesse', 'cpf': '008.192.019-12', 'endereco': 'rua 12 Vinhais casa 34 São Luís-MA', 'cep': '65000-000', 'email': 'Jesse@gmail.com', 'telefone': '9189-1920'},
+            { 'id': 2, 'nome': 'John', 'cpf': '008.192.019-12', 'endereco': 'rua 12 Vinhais casa 34 São Luís-MA', 'cep': '65000-000', 'email': 'John@gmail.com', 'telefone': '9189-1920'},
+            { 'id': 3, 'nome': 'Tina', 'cpf': '008.192.019-12', 'endereco': 'rua 12 Vinhais casa 34 São Luís-MA', 'cep': '65000-000', 'email': 'Tina@gmail.com', 'telefone': '9189-1920'},
+            { 'id': 4, 'nome': 'Clarence', 'cpf': '008.192.019-12', 'endereco': 'rua 12 Vinhais casa 34 São Luís-MA', 'cep': '65000-000', 'email': 'Clarence@gmail.com', 'telefone': '9189-1920'},
+            { 'id': 5, 'nome': 'Anne', 'cpf': '008.192.019-12', 'endereco': 'rua 12 Vinhais casa 34 São Luís-MA', 'cep': '65000-000', 'email': 'Anne@gmail.com', 'telefone': '9189-1920' }
           ],
           colunas: [
             {
               field: 'id',
-              label: 'ID',
+              label: 'Código',
               width: '40',
               numeric: true
             },
             {
-              field: 'first_name',
-              label: 'First Name',
+              searchable: true,
+              field: 'nome',
+              label: 'Nome',
             },
             {
-              field: 'last_name',
-              label: 'Last Name',
-            },
-            {
-              field: 'date',
-              label: 'Date',
-              centered: true
-            },
-            {
-              field: 'gender',
-              label: 'Gender',
+              field: 'telefone',
+              label: 'Telefone',
             }
           ]
         }
