6f7ebc116a3323879f16f14b8857de8d4a8734de
JosePaulo95 <fernandes.josepaulo95@gmail.com>
2020-09-09 08:50:32 -0300

    feat: import datadatables para gerar excel, botão no menu e config

---
+++ frontend/js/plugins/DataTable/initialization.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('.table').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            'excelHtml5'
+        ]
+    } );
+    $("#DataTables_Table_0_filter").hide();
+
+    let bt_excel = $(".dt-button.buttons-excel.buttons-html5");
+
+    $(".botoes-data-table").append('<div class="level-item"></div>')
+    $(".botoes-data-table .level-item").append(bt_excel)
+    bt_excel.append('\
+    	<span class="icon">\
+    		<i class="fa fa-download"></i>\
+  		</span>\
+  		')
+	bt_excel.removeClass("dt-button buttons-html5");
+	bt_excel.addClass("button is-small");
+} );
+
